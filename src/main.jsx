378fe99be8165c6dff0d3d5675fcb1b6f354fba9
Ahmad Jalil <ahzs645@gmail.com>
2026-01-17 10:48:48 -0800

    feat: wrap App component with StyleSheetManager for improved CSS handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { StyleSheetManager } from 'styled-components';
 import { ConfigProvider } from './contexts/ConfigContext';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ConfigProvider>
-      <App />
-    </ConfigProvider>
+    <StyleSheetManager disableCSSOMInjection={import.meta.env.PROD}>
+      <ConfigProvider>
+        <App />
+      </ConfigProvider>
+    </StyleSheetManager>
   </React.StrictMode>
 );
